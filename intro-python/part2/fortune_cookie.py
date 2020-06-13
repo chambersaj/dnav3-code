8dc4bd76220f57d62582292a107855177240b986
AJ Chambers <anchambe@cisco.com>
2020-06-12 21:34:10 -0400

    Hands on Part2 done

--- intro-python/part2/fortune_cookie.py
+++ intro-python/part2/fortune_cookie.py
@@ -34,8 +34,12 @@ def create_fortune_cookie_message(how_many_lucky_numbers: int) -> str:
     # TODO: Create a fortune cookie message by calling generate_fortune() and
     # generate_lucky_numbers() and then composing and returning the fortune
     # cookie's message.
+    lucky_numbers = generate_lucky_numbers(how_many_lucky_numbers)
+    fortune = generate_fortune()
+    fortune_cookie_message = 'Your lucky numbers are: ' + str(lucky_numbers) + ' your fortune is "' + fortune + '"'
+    return fortune_cookie_message
 
-    raise NotImplementedError()
+    
 
 
 def main():
